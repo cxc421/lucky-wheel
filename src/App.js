13eba67b8153da43b424f7560f9e2df8ca4e1a26
Chris Chuang <chrischuang0421@gmail.com>
2019-03-11 22:19:29 +0800

    Add toogle mode dropdown

--- src/App.js
+++ src/App.js
@@ -2,33 +2,35 @@
 //  App: connect all components
 //
 import React, { useState } from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import { MdSettings } from 'react-icons/md';
 
 import './global.css';
 import styles from './App.module.css';
 import WheelPage from './pages/WheelPage';
+import SetupPage from './pages/SetupPage';
 
 const config20 = [
-  // {
-  //   text: 'Movie',
-  //   mdIconName: 'MdMovieFilter',
-  //   total: 5
-  // },
-  // {
-  //   text: 'Wish',
-  //   mdIconName: 'MdCake',
-  //   total: 5
-  // },
-  // {
-  //   text: 'Anything',
-  //   mdIconName: 'MdStars',
-  //   total: 5
-  // },
-  // {
-  //   text: 'Child',
-  //   mdIconName: 'MdChildCare',
-  //   total: 4
-  // },
+  {
+    text: 'Movie',
+    mdIconName: 'MdMovieFilter',
+    total: 5
+  },
+  {
+    text: 'Wish',
+    mdIconName: 'MdCake',
+    total: 5
+  },
+  {
+    text: 'Anything',
+    mdIconName: 'MdStars',
+    total: 5
+  },
+  {
+    text: 'Child',
+    mdIconName: 'MdChildCare',
+    total: 4
+  },
   {
     text: 'Flight',
     mdIconName: 'MdFlight',
@@ -46,22 +48,41 @@ const MODE_SETTING = 'mode-setting';
 
 const App = () => {
   const [prizes, setPrizes] = useState([...config20]);
-  // const [mode, setMode] = useState(MODE_WHEEL);
+  const [mode, setMode] = useState(MODE_SETTING);
 
   return (
     <div className={styles.app}>
-      <DropdownButton
-        id="dropdown-basic-button"
-        title="Dropdown button"
-        variant="info"
-        className={styles.dropDown}
-      >
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </DropdownButton>
+      <Dropdown>
+        <Dropdown.Toggle
+          variant="info"
+          id="dropdown-basic"
+          className={styles.dropDown}
+        >
+          <MdSettings size={36} style={{ paddingRight: 10 }} />
+        </Dropdown.Toggle>
 
-      <WheelPage prizes={prizes} setPrizes={setPrizes} />
+        <Dropdown.Menu>
+          {[
+            { mode: MODE_WHEEL, text: 'Lucky Wheel' },
+            { mode: MODE_SETTING, text: 'Edit Config' }
+          ].map((item, idx) => (
+            <Dropdown.Item
+              href="#"
+              key={idx}
+              style={mode === item.mode ? { background: 'lightgrey' } : null}
+              onClick={() => setMode(item.mode)}
+            >
+              {item.text}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {mode === MODE_WHEEL ? (
+        <WheelPage prizes={prizes} setPrizes={setPrizes} />
+      ) : (
+        <SetupPage />
+      )}
     </div>
   );
 };
---
+++ src/pages/SetupPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SetupPage.module.css';
+
+const SetupPage = () => {
+  return <div className={styles.page} />;
+};
+
+export default SetupPage;
