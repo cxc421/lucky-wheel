361bf6beea7e0240dec7dc2475a599eb29e0bcb0
Chris Chuang <chrischuang0421@gmail.com>
2019-03-12 18:24:18 +0800

    Updates

---
+++ precache-manifest.74001543d6d8465478143bf302da7bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "838f2ced7641add5e19d5ffa86900488",
+    "url": "/lucky-wheel/static/media/hand.838f2ced.svg"
+  },
+  {
+    "revision": "2e4391d447760bcc16b1",
+    "url": "/lucky-wheel/static/js/runtime~main.f96c901a.js"
+  },
+  {
+    "revision": "0badbe4b8b1802402200",
+    "url": "/lucky-wheel/static/js/main.5d715ba9.chunk.js"
+  },
+  {
+    "revision": "24db98ca1e71e03b5579",
+    "url": "/lucky-wheel/static/js/2.dbc70492.chunk.js"
+  },
+  {
+    "revision": "0badbe4b8b1802402200",
+    "url": "/lucky-wheel/static/css/main.82dc59c5.chunk.css"
+  },
+  {
+    "revision": "1e8ab575d60d250142cbc57779f6afe7",
+    "url": "/lucky-wheel/index.html"
+  }
+];
